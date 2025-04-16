8d482b09537ac3343f0f80141610a143b382f3bf
akgunege1 <233301082@ogr.selcuk.edu.tr>
2025-04-16 17:13:00 +0300

    CSS Adjustments, Fine tuning.

--- employees-react/src/App.jsx
+++ employees-react/src/App.jsx
@@ -21,7 +21,7 @@ function Header() {
       <div>
         <ul className="team">
           <li className="cofounder">
-            <div className="thumb"><img src="Img/Kardesim.png" alt="Canim Kardesim" /></div>
+            <div className="myThumb"><img src="Img/Kardesim.png" alt="Canim Kardesim" /></div>
             <div className="description">
               <h3>Berat Ege Akgün</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, cupiditate.</p>
