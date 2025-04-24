70904863c5b49a0cd9a7890e067f0825b700fe42
akgunege1 <233301082@ogr.selcuk.edu.tr>
2025-04-24 19:30:49 +0300

    Add Modal is working completely. Can be quittable and submittable. Edit Modal is next.

--- employees-react/src/components/addModal.jsx
+++ employees-react/src/components/addModal.jsx
@@ -1,135 +1,127 @@
- import React, { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
-export const AddModal = (addEmployee) => {
+function AddModal({ onAddEmployee, setIsAddModalOpen }) {
+    const [formData, setFormData] = useState({
+        name: '',
+        mail: '',
+        adress: '',
+        phone: ''
+    });
 
-        
-        const [isAddModalOpen, setIsAddModalOpen] = useState(false)
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    }
 
-        const [formData, setFormData]= useState({
-            name:'',
-            mail:'',
-            adress:'',
-            phone:''    
-            }
-        )
+    function handleCancel() {
+        setIsAddModalOpen(false);
+        setFormData({
+            name: "",
+            mail: "",
+            adress: "",
+            phone: ""
+        });
+    }
 
-        const closeModal = () => setIsAddModalOpen(false)
+    function handleSubmit(e) {
+        e.preventDefault();
+        onAddEmployee(formData);
+        setIsAddModalOpen(false);
+        setFormData({
+            name: "",
+            mail: "",
+            adress: "",
+            phone: ""
+        });
+    }
 
-        function handleChange(event) {
-            const { name, value } = event.target;
-            setFormData((prevData) => ({
-              ...prevData,
-              [name]: value
-            }));
-          }
-
-        function handleSubmit(e){
-            e.preventDefault();
-            addEmployee(formData);
-            console.log('Form Gönderildi', formData);
-            closeModal();
-            setFormData({
-                name:'',
-                mail:'',
-                adress:'',
-                phone:''
-            })
-            }
-
-            function handleCancel(){
-                closeModal();
-                setFormData({
-                 name: "",
-                 mail: "",
-                 adress: "",
-                 phone: ""
-               });
-            }
-            {isAddModalOpen &&(
-
-
-            )}
-            return (
+    return (
         <div className="modal fade show d-block" tabIndex="-1">
-    <div className="modal-dialog">
-        <div className="modal-content">
-            <form onSubmit={() => {}}>
-                <div className="modal-header">
-                    <h5 className="modal-title">Add Employee</h5>
-                    <button
-                        type="button"
-                        className="close"
-                        onClick={handleCancel()}
-                    >
-                        <span>&times;</span>
-                    </button>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <form onSubmit={handleSubmit}>
+                        <div className="modal-header">
+                            <h5 className="modal-title">Add Employee</h5>
+                            <button
+                                type="button"
+                                className="close"
+                                onClick={handleCancel}
+                            >
+                                <span>&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    required
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    required
+                                    name="mail"
+                                    value={formData.mail}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Address</label>
+                                <textarea
+                                    className="form-control"
+                                    required
+                                    name="adress"
+                                    value={formData.adress}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Phone</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    required
+                                    name="phone"
+                                    value={formData.phone}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                onClick={handleCancel}
+                            >
+                                Close
+                            </button>
+                            <button type="submit"
+                                    className="btn btn-primary"
+                                    >
+                                Save
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <div className="modal-body">
-                    <div className="form-group">
-                        <label>Name</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            required
-                            name="name"
-                            value={() => {}}
-                            onChange={handleChange}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            required
-                            name="mail"
-                            value={() => {}}
-                            onChange={handleChange}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Address</label>
-                        <textarea
-                            className="form-control"
-                            required
-                            name="adress"
-                            value={() => {}}
-                            onChange={handleChange}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Phone</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            required
-                            name="phone"
-                            value={() => {}}
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
-
-                <div className="modal-footer">
-                    <button
-                        type="button"
-                        className="btn btn-secondary"
-                        onClick={handleCancel}
-                    >
-                        Close
-                    </button>
-                    <button type="submit" 
-                            className="btn btn-primary"
-                            onClick={handleSubmit}>
-                        Save
-                    </button>
-                </div>
-            </form>
+            </div>
         </div>
-    </div>
-</div>
-  )
+    )
 }
+
+export default AddModal;
--- employees-react/src/components/header.jsx
+++ employees-react/src/components/header.jsx
@@ -1,39 +1,38 @@
-import { AddModal } from "./addModal";
 import { useState } from "react";
+import AddModal from "./AddModal"
 
-function Header() {
-const[isAddModalOpen, setIsAddModalOpen]= useState(false);  
+function Header({ onAddEmployee }) {
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);  
 
-const openModal = () => setIsAddModalOpen(true)
-const closeModal = () => setIsAddModalOpen(false)
+    return (
+        <div>
+            <div className="table-title">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h2>
+                            Manage<b>Employees</b>
+                        </h2>
+                    </div>
+                    <div className="col-sm-6">
+                        <button onClick={() => setIsAddModalOpen(true)} className="btn btn-success">
+                            <i className="material-icons">&#xE147;</i>{" "}
+                            <span>Add New Employee</span>
+                        </button>
+                        <button onClick={() => setIsAddModalOpen(false)} className="btn btn-danger">
+                            <i className="material-icons">&#xE15C;</i> <span>Delete</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
 
-
-  return (
-    <div>
-      <div className="table-title">
-        <div className="row">
-          <div className="col-sm-6">
-            <h2>
-              Manage<b>Employees</b>
-            </h2>
-          </div>
-          <div className="col-sm-6">
-            <button onClick={openModal} className="btn btn-success">
-              <i className="material-icons">&#xE147;</i>{" "}
-              <span>Add New Employee</span>
-            </button>
-            <button onClick={closeModal} className="btn btn-danger">
-              <i className="material-icons">&#xE15C;</i> <span>Delete</span>
-            </button>
-          </div>
+            {isAddModalOpen && (
+                <AddModal 
+                    onAddEmployee={onAddEmployee} 
+                    setIsAddModalOpen={setIsAddModalOpen}
+                />
+            )}
         </div>
-      </div>
-
-      {isAddModalOpen && (
-          <AddModal />
-  )}
-    </div>
-  );
+    );
 }
 
 export default Header;
